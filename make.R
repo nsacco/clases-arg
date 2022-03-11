332c9d9c42aa7c5e4a455acbd0a1f7037b4dd25e
Nicolás Sacco <nsaccozeballos@gmail.com>
2022-03-11 18:41:43 -0500

    cap3 revisado

--- make.R
+++ make.R
@@ -10,4 +10,4 @@ usethis::use_blank_slate()
 if (!require(pacman)) {install.packages("pacman", dependencies = T)}; library(pacman)
 
 # Load / Install Required Packages
-p_load(tidyverse, bookdown, foreign, devtools, ggplot2, flextable, devtools, grDevices, formatR)
+p_load(tidyverse, bookdown, foreign, devtools, ggplot2, flextable, devtools, grDevices, formatR, rmarkdown, kableExtra, eph)
