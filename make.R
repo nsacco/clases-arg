4eb36ef06f127b2e3f6aa172a29b5bc8b555497b
Nicolás Sacco <nsaccozeballos@gmail.com>
2022-03-22 14:37:19 -0400

    cap7rev

--- make.R
+++ make.R
@@ -10,6 +10,7 @@ usethis::use_blank_slate()
 if (!require(pacman)) {install.packages("pacman", dependencies = T)}; library(pacman)
 
 # Load / Install Required Packages
-p_load(tidyverse, bookdown, foreign, devtools, ggplot2, flextable, devtools, grDevices, formatR, rmarkdown, kableExtra, eph, vembedr, modeest, summarytools)
+p_load(tidyverse, bookdown, foreign, devtools, ggplot2, flextable, devtools, grDevices, formatR, rmarkdown, kableExtra, eph, vembedr, modeest, summarytools, extrafont, showtext)
 
 devtools::install_github("DiogoFerrari/occupar")
+
