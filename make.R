b1fabd225b32aa9026f09c0781bcbf4b0cbc0d45
Nicolás Sacco <nsaccozeballos@gmail.com>
2023-08-09 18:26:31 -0300

    nuevos cambios libro

--- make.R
+++ make.R
@@ -9,10 +9,15 @@ usethis::use_blank_slate()
 
 if (!require(pacman)) {install.packages("pacman", dependencies = T)}; library(pacman)
 
-# Load / Install Required Packages
-p_load(tidyverse, bookdown, foreign, devtools, ggplot2, flextable, devtools, grDevices, formatR, rmarkdown, kableExtra, eph, vembedr, modeest, summarytools, extrafont, showtext, jtools, ggthemes, knitr)
-
 devtools::install_github("DiogoFerrari/occupar")
+
+remotes::install_github("wilkelab/cowplot")
+install.packages("colorspace", repos = "http://R-Forge.R-project.org")
+remotes::install_github("clauswilke/colorblindr")
+
+# Load / Install Required Packages
+p_load(tidyverse, bookdown, foreign, devtools, ggplot2, flextable, devtools, grDevices, formatR, rmarkdown, kableExtra, eph, vembedr, modeest, summarytools, extrafont, showtext, jtools, ggthemes, knitr, car, occupar, huxtable, sf, colorblindr)
+
 
 
 
